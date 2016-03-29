5e537ad57e6929c167316a9eb81407cfd9eec04a
Sean McNamara <sean@coder.ie>
2016-03-28 23:42:45 -0700

    API Client + API Client Interface

--- src/php-represent/API.php
+++ src/php-represent/API.php
@@ -4,47 +4,183 @@ namespace PHPRepresent;
 
 use PHPRepresent\Interfaces\APIInterface;
 
-class API implements APIInterface{
+class API implements APIInterface
+{
 
-	public $apiUrl = 'https://represent.opennorth.ca';
+    protected $apiUrl = 'https://represent.opennorth.ca/';
+    protected $secure = true;
+    protected $ratePeriod = 60;
+    protected $rateLimit = 60;
+    protected $rateHistory = [];
 
-	public function setRateLimit() {
-		// TODO: Implement setRateLimit() method.
-	}
 
-	public function get( $path, array $params, $throttle = true ) {
-		// TODO: Implement get() method.
-	}
+    public function setInsecure()
+    {
+        $this->apiUrl = 'http://represent.opennorth.ca/';
+        $this->secure = false;
+    }
 
-	public function getAll( $path, array $params ) {
-		// TODO: Implement getAll() method.
-	}
+    public function setRateLimit($limit)
+    {
+        $this->rateLimit = $limit;
+    }
 
-	public function postcodes( $postcode = null ) {
-		// TODO: Implement postcodes() method.
-	}
+    public function rateLimit()
+    {
+        $begin     = new \DateTime(date("Y-m-d H:i:s", strtotime("1 minute ago")));
+        $end       = new \DateTime(date("Y-m-d H:i:s"));
+        $interval  = new \DateInterval('PT1S');
+        $dateRange = new \DatePeriod($begin, $interval, $end);
 
-	public function boundarySets( $name = null, array $params ) {
-		// TODO: Implement boundarySets() method.
-	}
+        $requestCount = 0;
+        foreach ($dateRange as $date) {
+            if (array_search($date->format("Y-m-d H:i:s"), $this->rateHistory)) {
+                $requestCount ++;
+            }
+        }
 
-	public function boundaries( $boundarySet = null, $name = null, $representatives = false, array $params ) {
-		// TODO: Implement boundaries() method.
-	}
+        if ($requestCount >= $this->rateLimit) {
+            error_log('Throttling Open North API calls');
+            sleep($this->ratePeriod);
+        }
 
-	public function representativeSets( $set = null ) {
-		// TODO: Implement representativeSets() method.
-	}
+        $rateHistory[] = date("Y-m-d H:i:s");
+    }
 
-	public function representatives( $set = null, array $params ) {
-		// TODO: Implement representatives() method.
-	}
+    public function get($path, array $params = [], $throttle = true)
+    {
+        if ($throttle) {
+            $this->rateLimit();
+        }
 
-	public function elections( $set = null ) {
-		// TODO: Implement elections() method.
-	}
+        $response = $this->simpleCurl($path, $params);
 
-	public function candidates( $set = null, array $params ) {
-		// TODO: Implement candidates() method.
-	}
+        return $response;
+    }
+
+    public function getAll($path, array $params = [])
+    {
+        $objects = [];
+
+        $params['limit'] = !array_key_exists('limit', $params) ? 20 : $params['limit'];
+        $params['offset'] = !array_key_exists('offset', $params) ? 0 : $params['offset'];
+
+        do {
+            $this->rateLimit();
+            $resultSet        = json_decode($this->simpleCurl($path, $params));
+            $more             = $resultSet->meta->next;
+            $objects          = array_merge($objects, $resultSet->objects);
+            $params['offset'] += $params['limit'];
+        } while ($more !== null);
+
+        return json_encode($objects);
+    }
+
+    public function postcode($postcode)
+    {
+        $path = 'postcodes/' . strtoupper($postcode);
+
+        return $this->get($path);
+    }
+
+    public function boundarySets($name = null, array $params = [])
+    {
+        $path = 'boundary-sets';
+        if ($name !== null) {
+            $path .= '/' . $name;
+
+            return $this->get($path, $params);
+        }
+
+        return $this->getAll($path, $params);
+    }
+
+    public function boundaries($boundarySet = null, $name = null, $representatives = false, array $params = [])
+    {
+        $path = 'boundaries';
+
+        if ($boundarySet !== null) {
+            $path .= '/' . $boundarySet;
+            if ($name !== null) {
+                $path .= '/' . $name;
+                if ($representatives) {
+                    $path .= '/representatives';
+                }
+            }
+        } else if(!array_key_exists('sets', $params) && !array_key_exists('limit', $params)) {
+            // Note: To get all boundaries we need to amp up the limit to not hit max execution time.
+            $params['limit'] = 1000;
+        }
+
+        return $this->getAll($path, $params);
+    }
+
+    public function representativeSets($set = null)
+    {
+        $path = 'representative-sets';
+        if($set !== null){
+            $path .= '/'.$set;
+            return $this->get($path);
+        }
+        return $this->getAll($path);
+    }
+
+    public function representatives($set = null, array $params = [])
+    {
+        // TODO: Implement representatives() method.
+    }
+
+    public function elections($set = null)
+    {
+        // TODO: Implement elections() method.
+    }
+
+    public function candidates($set = null, array $params = [])
+    {
+        // TODO: Implement candidates() method.
+    }
+
+    protected function simpleCurl($path, $params = [], $options = [])
+    {
+
+        $paramUrl = '';
+        $c        = 0;
+
+        foreach ($params as $key => $param) {
+            if (is_array($param)) {
+                $param = implode(",", $param);
+            }
+            if ($c == 0) {
+                $paramUrl .= '?' . $key . '=' . rawurlencode($param);
+            } else {
+                $paramUrl .= '&' . $key . '=' . rawurlencode($param);
+            }
+            $c ++;
+        }
+
+        $url = $this->apiUrl . $path . '/' . $paramUrl;
+
+        $defaults = [
+            CURLOPT_URL            => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT        => 30,
+            CURLOPT_CONNECTTIMEOUT => 15,
+        ];
+
+        if ($this->secure === false) {
+            $defaults[CURLOPT_SSL_VERIFYPEER] = false;
+            $defaults[CURLOPT_SSL_VERIFYHOST] = false;
+        }
+
+        $ch = curl_init();
+        curl_setopt_array($ch, ( $options + $defaults ));
+
+        if ( ! $result = curl_exec($ch)) {
+            var_dump(curl_error($ch));
+            error_log(curl_error($ch));
+        }
+        curl_close($ch);
+
+        return $result;
+    }
 }
--- src/php-represent/interfaces/APIInterface.php
+++ src/php-represent/interfaces/APIInterface.php
@@ -6,84 +6,97 @@ namespace PHPRepresent\Interfaces;
  * Interface APIInterface
  * @package PHPRepresent\Interfaces
  */
-interface APIInterface {
+interface APIInterface
+{
 
-	/**
-	 * @return mixed
-	 */
-	public function setRateLimit();
+    /**
+     * @return mixed
+     */
+    public function setInsecure();
 
-	/**
-	 * @param string $path
-	 * @param array $params
-	 * @param bool|true $throttle
-	 *
-	 * @return mixed
-	 */
-	public function get($path, array $params, $throttle = TRUE);
+    /**
+     * @param $limit
+     *
+     * @return mixed
+     */
+    public function setRateLimit($limit);
 
-	/**
-	 * @param $path
-	 * @param array $params
-	 *
-	 * @return mixed
-	 */
-	public function getAll($path, array $params);
+    /**
+     * @return mixed
+     */
+    public function rateLimit();
 
-	/**
-	 * @param null $postcode
-	 *
-	 * @return mixed
-	 */
-	public function postcodes($postcode = NULL);
+    /**
+     * @param string $path
+     * @param array $params
+     * @param bool|true $throttle
+     *
+     * @return mixed
+     */
+    public function get($path, array $params = [], $throttle = true);
 
-	/**
-	 * @param string|null $name
-	 * @param array $params
-	 *
-	 * @return mixed
-	 */
-	public function boundarySets($name = NULL, array $params);
+    /**
+     * @param $path
+     * @param array $params
+     *
+     * @return mixed
+     */
+    public function getAll($path, array $params = []);
+
+    /**
+     * @param string $postcode
+     *
+     * @return mixed
+     */
+    public function postcode($postcode);
+
+    /**
+     * @param string|null $name
+     * @param array $params
+     *
+     * @return mixed
+     */
+    public function boundarySets($name = null, array $params = []);
 
 
-	/**
-	 * @param string|null $boundarySet
-	 * @param string|null $name
-	 * @param bool|false $representatives
-	 * @param array $params
-	 *
-	 * @return mixed
-	 */
-	public function boundaries($boundarySet = NULL, $name = NULL, $representatives = FALSE, array $params);
+    /**
+     * @param string|null $boundarySet
+     * @param string|null $name
+     * @param bool|false $representatives
+     * @param array $params
+     *
+     * @return mixed
+     */
+    public function boundaries($boundarySet = null, $name = null, $representatives = false, array $params = []);
 
-	/**
-	 * @param null $set
-	 *
-	 * @return mixed
-	 */
-	public function representativeSets($set = NULL);
+    /**
+     * @param null $set
+     *
+     * @return mixed
+     */
+    public function representativeSets($set = null);
 
-	/**
-	 * @param null $set
-	 * @param array $params
-	 *
-	 * @return mixed
-	 */
-	public function representatives($set = NULL, array $params);
+    /**
+     * @param null $set
+     * @param array $params
+     *
+     * @return mixed
+     */
+    public function representatives($set = null, array $params = []);
 
-	/**
-	 * @param null $set
-	 *
-	 * @return mixed
-	 */
-	public function elections($set = NULL);
+    /**
+     * @param null $set
+     *
+     * @return mixed
+     */
+    public function elections($set = null);
 
-	/**
-	 * @param null $set
-	 * @param array $params
-	 *
-	 * @return mixed
-	 */
-	public function candidates($set = NULL, array $params);
+    /**
+     * @param null $set
+     * @param array $params
+     *
+     * @return mixed
+     */
+    public function candidates($set = null, array $params = []);
 
 }
